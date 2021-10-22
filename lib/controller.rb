10eecd1ec9fdbdb743a218a028f29e0b53580a83
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-22 14:54:23 +0200

    initial commit

---
+++ lib/controller.rb
@@ -0,0 +1,11 @@
+require 'gossip'
+require 'view'
+
+class Controller
+
+  def create_gossip
+    gossip = Gossip.new
+  end
+
+
+end
---
+++ lib/gossip.rb
@@ -0,0 +1,15 @@
+class Gossip
+
+  attr_reader :author, :content
+
+  def initialize(author, content)
+    @author = author
+    @content = content
+  end
+
+
+  def save(gossip)
+
+  end
+
+end
